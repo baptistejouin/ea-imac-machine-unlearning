a5d06c3d032ed32f8c43958a311ade2e0b5c7191
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-25 22:29:27 +0100

    wip

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,7 +1,55 @@
+let classifier, img, canvas;
+const canvaSize = 500;
+const resultDOM = document
+  .querySelector("#result-remplate-js")
+  .content.cloneNode(true);
+
+const images = {
+  boat: "assets/images/boat.jpeg",
+  bird: "assets/images/mésange_azurée.jpeg",
+};
+const PARAMS = {
+  currentImage: "assets/images/boat.jpeg",
+};
+
 // The GUI
-var GUI = lil.GUI;
-const gui = new GUI();
-gui.add(document, "title");
-obj = { number1: 0.533, number2: 50 };
-gui.add(obj, "number1", 0, 1); // min, max
-gui.add(obj, "number2", 0, 100, 10); // min, max, step
+const gui = new lil.GUI();
+gui.add(PARAMS, "currentImage").options(Object.keys(images));
+
+function preload() {
+  //Models available are: 'MobileNet', 'Darknet' and 'Darknet-tiny','DoodleNet'...
+  classifier = ml5.imageClassifier("MobileNet");
+  img = loadImage(PARAMS.currentImage);
+}
+
+function setup() {
+  canvas = createCanvas(canvaSize, canvaSize);
+  background(200);
+  noLoop();
+
+  image(img, 0, 0, canvaSize, canvaSize);
+
+  classifier.classify(canvas, displayResult);
+}
+
+function displayResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(results);
+
+  const body = document.querySelector("body");
+  const notice = resultDOM.querySelector(".notice");
+  notice.innerHTML = "";
+
+  for (const result of results) {
+    notice.innerHTML += `<div>${result.label} (${nf(
+      results[0].confidence,
+      0,
+      2
+    )})`;
+  }
+  body.appendChild(resultDOM);
+}
