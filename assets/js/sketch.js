c64dbc790a8153a0033c59eea666f6b9e04165cb
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-26 14:34:01 +0200

    feat: add gui and image processing

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,48 +1,64 @@
-let classifier, img, canvas;
+// Our images (add path our new image here)
+const images = {
+  boat: "assets/images/boat.jpeg",
+  bird: "assets/images/mésange_azurée.jpeg",
+  sunflower: "assets/images/sunflower.jpeg",
+  polar_bear: "assets/images/polar_bear.jpeg",
+};
+
+// Essential variables for our program
+let classifier,
+  canvas,
+  img = [];
 const canvaSize = 500;
 const resultDOM = document
   .querySelector("#result-remplate-js")
   .content.cloneNode(true);
-
-const images = {
-  boat: "assets/images/boat.jpeg",
-  bird: "assets/images/mésange_azurée.jpeg",
-};
+const body = document.querySelector("body");
 const PARAMS = {
-  currentImage: "assets/images/boat.jpeg",
+  currentImage: Object.keys(images)[0], // Define here the index of the selected default image
 };
 
-// The GUI
+// GUI with the "lil GUI" library
 const gui = new lil.GUI();
-gui.add(PARAMS, "currentImage").options(Object.keys(images));
+gui
+  .add(PARAMS, "currentImage")
+  .options(Object.keys(images))
+  .onChange(() => redraw());
 
 function preload() {
   //Models available are: 'MobileNet', 'Darknet' and 'Darknet-tiny','DoodleNet'...
   classifier = ml5.imageClassifier("MobileNet");
-  img = loadImage(PARAMS.currentImage);
+
+  for (const key in images) {
+    if (Object.hasOwnProperty.call(images, key)) {
+      img[key] = loadImage(images[key]);
+    }
+  }
 }
 
 function setup() {
   canvas = createCanvas(canvaSize, canvaSize);
   background(200);
   noLoop();
+}
 
-  image(img, 0, 0, canvaSize, canvaSize);
-
+function draw() {
+  image(img[PARAMS.currentImage], 0, 0, canvaSize, canvaSize);
   classifier.classify(canvas, displayResult);
 }
 
 function displayResult(error, results) {
+  const notice = resultDOM.querySelector(".notice");
+
+  notice.innerHTML = "";
+
   if (error) {
     console.error(error);
     return;
   }
 
   console.log(results);
-
-  const body = document.querySelector("body");
-  const notice = resultDOM.querySelector(".notice");
-  notice.innerHTML = "";
 
   for (const result of results) {
     notice.innerHTML += `<div>${result.label} (${nf(
@@ -51,5 +67,6 @@ function displayResult(error, results) {
       2
     )})`;
   }
+
   body.appendChild(resultDOM);
 }
